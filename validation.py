f894122bd2beaecbf392c6867971752666b9c15a
Raul Poma <131233898+23958495@users.noreply.github.com>
2023-05-02 12:30:48 +0800

    Add Strategy
    I added the strategy and reconfigured the bot to show the trades.
    
    Needs to check the occurrences of buy and sell.

--- validation.py
+++ validation.py
@@ -1,57 +1,68 @@
-import backtrader as bt
 import pandas as pd
+import datetime
 
 # Here is for the trading strategy, after we generate it, we can fill this part
-class MyStrategy(bt.Strategy):
-    ...
+class TEMA(bt.Strategy):
+    params = dict(ema1=12,ema2=26,ema3=48)
+
+    def __init__(self):
+
+        self.small_p = bt.indicators.EMA(self.data.close, period=self.p.ema1)
+        self.medium_p = bt.indicators.EMA(self.data.close, period=self.p.ema2)
+        self.large_p = bt.indicators.EMA(self.data.close, period=self.p.ema3)
+        
+    def next(self):
+        if self.small_p > self.medium_p and self.small_p > self.large_p:
+            self.buy()
+        elif self.small_p < self.medium_p and self.small_p < self.large_p:
+            self.sell()
 
 
-def backtest(data_file, strategy_class, commission_rate=0.02, initial_cash=100):
+def read_datafile(data_file):
+    df = pd.read_csv(data_file, index_col=1, parse_dates=True).sort_index()
+    df.index = pd.to_datetime(df.index, unit='s')
+    return df[["open","high","low","close"]].round(4)
+
+
+
+def backtest(data_file,**strategy_params):
     # Read the data, here the 'data_file' would be the path of the csv file
-    data = pd.read_csv(data_file, parse_dates=['unixtimestap'], index_col='unixtimestap')
+    #data = pd.read_csv(data_file,index_col=1,parse_dates=True)
+    test=read_datafile(data_file)
     # may need to do some data selection
-
     # create a Backtrader engine
+    #print(data['unixtimestap'])
     cerebro = bt.Cerebro()
 
     # add the data into the engine
-    data_feed = bt.feeds.PandasData(dataname=data)
+    data_feed = bt.feeds.PandasData(dataname=test)
+
     cerebro.adddata(data_feed)
 
+    cerebro.broker.setcash(100.0)
+
+    # Print out the starting conditions
+    print(f"Starting Portfolio Value: {cerebro.broker.getvalue():,.2f}")
+
     # add the trading strategy into the engine
-    cerebro.addstrategy(strategy_class, ...)
+    cerebro.addstrategy(TEMA, **strategy_params)
 
     # set the handling fee, it will be 2%
-    cerebro.broker.setcommission(commission=commission_rate)
+    cerebro.broker.setcommission(commission=0.0025,margin=False)
+    cerebro.addobserver(bt.observers.Trades)
+    cerebro.addobserver(bt.observers.DrawDown)
+    cerebro.addanalyzer(bt.analyzers.SharpeRatio)
 
     # set the initial cash
-    cerebro.broker.setcash(initial_cash)
+    #cerebro.broker.setcash(initial_cash)
 
     # run the engine
-    result = cerebro.run()
+    strats = cerebro.run()
 
-    return result[0]
+    print(f"Final Portfolio Value:    {cerebro.broker.getvalue():,.2f}")
+    cerebro.plot(volume=False)
 
 
-def analyze_backtest(result):
-    # print the validation result
-    final_value = result.broker.getvalue()
-    initial_value = result.broker.startingcash
-    profit = (final_value - initial_value) / initial_value
-    print(f"Initial Value: {initial_value:.2f} AUD")
-    print(f"Final Value: {final_value:.2f} AUD")
-    print(f"Profit: {profit:.2%}")
+STRATEGY_PARAMS = dict(ema1=2, ema2=6, ema3=10)
+back = backtest('data/kraken_data.csv',**STRATEGY_PARAMS)
 
-    # plot the candle chart
-    cerebro.plot(style='candle')
-
-    # Print the trading details
-    for trade in result.trade_history:
-        print(trade)
-
-    # Print other result like trading times and winning rate
-    trade_count = len(result.trade_history)
-    win_count = len([t for t in result.trade_history if t.pnlcomm > 0])
-    win_rate = win_count / trade_count
-    print(f"Trading times: {trade_count}")
-    print(f"Winning rate: {win_rate:.2%}")
